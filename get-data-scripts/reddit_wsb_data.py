6666e9f50c147543b824b40486d61a804b448a98
jobu9395 <jburdett1993@gmail.com>
2022-04-09 15:35:45 -0500

    scaffolding

---
+++ get-data-scripts/reddit_wsb_data.py
@@ -0,0 +1,37 @@
+import os
+import praw
+import pandas as pd
+
+
+CLIENT_ID = os.environ.get('reddit_client_id')
+CLIENT_SECRET = os.environ.get('reddit_client_secret')
+USER_AGENT = os.environ.get('reddit_user_agent')
+
+
+print(CLIENT_ID)
+
+# sets up reddit client
+reddit = praw.Reddit(client_id=CLIENT_ID,
+                     client_secret=CLIENT_SECRET,
+                     user_agent=USER_AGENT)  ## to use this, make a Reddit app. Client ID is in top left corner, client secret is given, and user agent is the username that the app is under
+
+subreddit = reddit.subreddit("wallstreetbets")
+
+# sets up
+payload = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+print(payload.head())
+first_table = payload[0]
+second_table = payload[1]
+df = first_table
+ordered_list_of_tickers = df[df.columns[0]]
+string_list_of_tickers = []
+for element in ordered_list_of_tickers:
+    string_list_of_tickers.append('' + element + '')
+
+
+# print(string_list_of_tickers)
+ordered_list_of_tickers.to_csv('ticker_list.csv', index=False)
+
+
+def ticker_list():
+    return string_list_of_tickers
