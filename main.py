2f7a5a8066ea1b27a5981d68f84dccb9afe284bf
bbursey <bbursey3@gatech.edu>
2022-04-12 21:09:09 -0400

    fix main method

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from get_data_scripts.reddit_data import get_post_statistics
 
-if __name__ == "__main__":
-    
-    # saves reddit posts and comments into datasets
+
+def main():
     get_post_statistics("wallstreetbets")
     get_post_statistics("investing")
+
+if __name__ == "__main__":
+    main()
