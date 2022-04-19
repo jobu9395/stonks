b5d9d5ecc4f1e14fe9c96bb80da31fae5cc62400
Josh Burdett <jburdett1993@gmail.com>
2022-04-18 23:10:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from get_data_scripts.reddit_data import get_post_statistics
 # from get_data_scripts.yahoo_finance_data import get_daily_stock_prices
 from scripts.training_join import join_yfinance_with_reddit_comments, aggregate_sentiment_scores
 from get_data_scripts.yfinance_all_price_data import get_daily_stock_prices
-# from models.rnn import train_model
+from models.rnn import train_model
 
 
 STOCKS = [
